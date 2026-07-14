6115552fd284042bd90628e86bd5db6ec9271672
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2026-07-14 13:43:47 +0600

    server folder structured and signup api constructed

---
+++ server/src/index.js
@@ -0,0 +1,60 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import { pool } from './db.js'
+import bcrypt from 'bcrypt'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/api/health', (req, res) => {
+    res.json({
+        status: 'ok',
+        message: 'Code Station server is healthy'
+    })
+})
+
+
+app.get('/api/test-db', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT NOW()')
+        res.json({ status: 'ok', message: 'Database connected!', time: result.rows[0] })
+    } catch (error) {
+        res.status(500).json({ status: 'error', message: error.message })
+    }
+})
+
+
+app.post('/api/signup', async (req, res) => {
+  try {
+    const { name, email, password } = req.body
+    const passwordHash = await bcrypt.hash(password, 10)
+
+    const result = await pool.query(
+      'INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3) RETURNING id, name, email, role, created_at',
+      [name, email, passwordHash]
+    )
+
+    res.status(201).json({
+      status: 'ok',
+      user: result.rows[0]
+    })
+  } catch (error) {
+    res.status(500).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is online! Running on PORT:${PORT}`)
+})
