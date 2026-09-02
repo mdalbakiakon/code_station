a8991e83f0a3a6bc7b2e72e71a1ba73d242bf220
Md. Al Baki Akon <shahjadsiam512@gmail.com>
2026-09-02 21:29:36 +0600

    fix: cleaned up old code and introduced root variable for more scale

--- client/src/components/LandingNav.jsx
+++ client/src/components/LandingNav.jsx
@@ -4,38 +4,38 @@ import { Link, NavLink } from "react-router-dom";
 
 const LandingNav = () => {
   return (
-    <header className="w-[calc(100%-20px)] md:w-[calc(100%-40px)] max-w-7xl h-12 mx-auto bg-black rounded-xl p-1 flex justify-between items-center fixed top-5 left-1/2 -translate-x-1/2 z-50 text-[#f5f5f5] leading-none tracking-tighter">
+    <header className="w-[calc(100%-20px)] md:w-[calc(100%-40px)] max-w-7xl h-12 mx-auto bg-(--land-bg-main) rounded-xl p-1 flex justify-between items-center fixed top-5 left-1/2 -translate-x-1/2 z-50 text-(--land-txt-main) leading-none tracking-tighter">
       
       <span className="text-lg md:text-xl font-ex relative left-0 md:left-2.5 text-left">CodeStation</span>
 
-      <nav className="hidden md:flex justify-center items-center font-lt gap-7.5">
+      <nav className="hidden md:flex justify-center items-center font-rg gap-7.5">
         <NavLink
           to="/"
-          className={({ isActive }) => (isActive ? "text-[#707070] font-ex" : "")}
+          className={({ isActive }) => (isActive ? "text-(--land-txt-sec) font-ex" : "")}
         >
           Home
         </NavLink>
         <NavLink
           to="/courses"
-          className={({ isActive }) => (isActive ? "text-[#707070] font-ex" : "")}
+          className={({ isActive }) => (isActive ? "text-(--land-txt-sec) font-ex" : "")}
         >
           Courses
         </NavLink>
         <NavLink
           to="/instructors"
-          className={({ isActive }) => (isActive ? "text-[#707070] font-ex" : "")}
+          className={({ isActive }) => (isActive ? "text-(--land-txt-sec) font-ex" : "")}
         >
           Instructors
         </NavLink>
         <NavLink
           to="/testimonials"
-          className={({ isActive }) => (isActive ? "text-[#707070] font-ex" : "")}
+          className={({ isActive }) => (isActive ? "text-(--land-txt-sec) font-ex" : "")}
         >
           Testimonials
         </NavLink>
       </nav>
 
-      <button className="h-full px-4.5 rounded-lg bg-[#f5f5f5] text-[#111] font-ex hidden md:flex justify-center items-center leading-0 cursor-pointer">
+      <button className="h-full px-4.5 rounded-lg bg-(--land-txt-main) text-(--land-bg-main) font-ex hidden md:flex justify-center items-center leading-0 cursor-pointer">
         Signup
       </button>
 
